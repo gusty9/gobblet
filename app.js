708affd7849321b8971520c0f37dc8b7fab3a9eb
gusty9 <gustafson.93@osu.edu>
2020-12-19 17:19:34 -0500

    added socket io

--- app.js
+++ app.js
@@ -1,14 +1,24 @@
 const express = require('express');
 const app = express();
+const server = require('http').createServer(app);
+const options = {/**/};
+const io = require('socket.io')(server, options);
 const path = require('path');
 const port = 8080;
 //set up the static resources directory
 app.use(express.static('static'));
 
+/**
+ * HTTP GET endpoint for the index webpage
+ */
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname + '/static/index.html'));
 });
 
-app.listen(port, () => {
+io.on('connection', socket => {
+    console.log('connected!');
+});
+
+server.listen(port, () => {
     console.log(`App listening on port ${port}`);
 });
--- static/index.js
+++ static/index.js
@@ -1,4 +1,28 @@
-let local_2_player = document.querySelector('#local_button');
-local_2_player.addEventListener('click', () => {
+//views
+let local_button = document.querySelector('#local_button');
+let create_match_button = document.querySelector('#create_match');
+let join_match_button = document.querySelector('#join_match');
+let room_id_text = document.querySelector('#room_id');
+
+//sockets
+const socket = io();
+
+socket.on('connect', () => {
+    console.log('connected callback');
+});
+
+//click listeners
+//redirect the user to a local 2 player version of goblet
+local_button.addEventListener('click', () => {
     window.location.href = '/local.html';
+});
+
+//create a new online match
+create_match_button.addEventListener('click', () => {
+
+});
+
+//join an online match
+join_match_button.addEventListener('click', () => {
+    let id = room_id_text.value;
 });
